64af32493647cb4d4650f4e9a83b86e38460d426
Ryan P. Brewster <RyanPBrewster@gmail.com>
2022-02-26 00:48:04 -0500

    rework to look more like qdigest

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,26 @@
-use std::collections::{BTreeMap, BinaryHeap};
+use std::collections::BTreeMap;
 
 #[derive(Default, Debug)]
 pub struct QDigest {
     root: Node,
+    total_weight: u64,
 }
 
 impl QDigest {
     pub fn insert(&mut self, key: &[u8], weight: u64) {
-        let mut node = &mut self.root;
-        for &b in key {
-            node.weight += weight;
-            node = node.children.entry(b).or_default();
-        }
-        node.weight += weight;
+        self.total_weight += weight;
+        self.root.insert(key, weight);
     }
 
-    pub fn compress(&mut self, mut gas: usize) {
-        let mut q = BinaryHeap::new();
-        q.push(&mut self.root);
-        while let Some(node) = q.pop() {
-            if node.children.len() > gas {
-                node.children.clear();
-                continue;
-            }
-            gas -= node.children.len();
-            for child in node.children.values_mut() {
-                q.push(child);
-            }
-        }
+    pub fn compress(&mut self, k: u64) {
+        self.root.compress(self.total_weight / k);
     }
 
-    pub fn boundaries(&self) -> Vec<Vec<u8>> {
-        let mut acc = Vec::new();
+    pub fn quantile(&self, p: f64) -> Vec<u8> {
         let mut cur = Vec::new();
-        boundaries_helper(&mut acc, &mut cur, &self.root);
-        acc.reverse();
-        acc
-    }
-}
-
-// Does a post-order traversal of the trie.
-fn boundaries_helper(acc: &mut Vec<Vec<u8>>, cur: &mut Vec<u8>, node: &Node) {
-    let mut total = 0;
-    for (&b, child) in node.children.iter().rev() {
-        total += child.weight;
-        cur.push(b);
-        boundaries_helper(acc, cur, child);
-        cur.pop();
-    }
-    if total < node.weight {
-        acc.push(cur.clone());
+        let mut gas = (self.total_weight as f64 * p) as u64;
+        explore(&self.root, &mut gas, &mut cur);
+        cur
     }
 }
 
@@ -58,22 +29,43 @@ struct Node {
     weight: u64,
     children: BTreeMap<u8, Node>,
 }
+impl Node {
+    fn insert(&mut self, key: &[u8], weight: u64) {
+        if key.is_empty() {
+            self.weight += weight;
+        } else {
+            self.children
+                .entry(key[0])
+                .or_default()
+                .insert(&key[1..], weight);
+        }
+    }
+    fn compress(&mut self, threshold: u64) {
+        let mut total = self.weight;
+        for child in self.children.values_mut() {
+            child.compress(threshold);
+            total += child.weight;
+        }
+        if total <= threshold {
+            self.children.retain(|_, child| {
+                child.weight = 0;
+                !child.children.is_empty()
+            });
+            self.weight = total;
+        }
+    }
+}
 
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.weight == other.weight
+fn explore(node: &Node, gas: &mut u64, key: &mut Vec<u8>) {
+    for (&b, child) in &node.children {
+        key.push(b);
+        explore(child, gas, key);
+        if *gas == 0 {
+            return;
+        }
+        key.pop();
     }
-}
-impl Eq for Node {}
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.weight.cmp(&other.weight)
-    }
+    *gas = gas.saturating_sub(node.weight);
 }
 
 #[cfg(test)]
@@ -84,14 +76,17 @@ mod tests {
     fn basic_smoke_test() {
         let mut h = QDigest::default();
         h.insert(b"0011", 9);
+        println!("{:?}\n", h);
         h.insert(b"0022", 9);
+        println!("{:?}\n", h);
         h.insert(b"AA11", 1);
+        println!("{:?}\n", h);
         h.insert(b"AA22", 1);
-        h.compress(8);
-        assert_eq!(
-            h.boundaries(),
-            vec![b"0011".to_vec(), b"0022".to_vec(), b"AA".to_vec()]
-        );
+        println!("{:?}\n", h);
+        h.compress(3);
+        println!("{:?}\n", h);
+        assert_eq!(h.quantile(0.1), b"0011".to_vec(),);
+        assert_eq!(h.quantile(0.5), b"0022".to_vec(),);
     }
 
     #[test]
@@ -102,9 +97,6 @@ mod tests {
         h.insert(b"AA11", 1);
         h.insert(b"AA22", 1);
         h.compress(7);
-        assert_eq!(
-            h.boundaries(),
-            vec![b"00".to_vec(), b"0011".to_vec(), b"AA".to_vec()]
-        );
+        assert_eq!(h.quantile(0.99), b"AA".to_vec(),);
     }
 }
